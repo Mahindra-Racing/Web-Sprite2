7ac1dccb33f0e40cf9bf26e941a54004b3e09c01
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-19 11:31:45 -0300

    V3

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
             <ul className="nav-list2">
                 <li>
                     <button className="btn-entrar">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--accent-clr)">
-                        <path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z"/>
-                    </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--accent-clr)">
+                            <path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z" />
+                        </svg>
                         Register
                     </button>
                 </li>
