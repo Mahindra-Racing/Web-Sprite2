13c85a2ed929655a387eba0b36470fba78e6475d
Vitor <vitoralveseskes@gmail.com>
2024-11-02 00:28:53 -0300

    V28 P1

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Sidebar.css';
+import './SideBar.css';
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import {
